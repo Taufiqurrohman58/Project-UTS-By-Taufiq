355c1cb540ca502fe0f8c39af3a78e9a84850c75
Taufiqurrohman58 <taufiqurrohman250503@gmail.com>
2024-05-03 01:33:42 -0700

    Menambahkan 3 Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,69 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Beranda from './screens/Beranda';
+import Profil from './screens/Profil';
+import Pengaturan from './screens/Pengaturan';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const MenuTab = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Tabs.Navigator screenOptions={{tabBarActiveTintColor: '#e91e63'}}>
+      <Tabs.Screen
+        name="Beranda"
+        component={Beranda}
+        options={{
+          tabBarLabel: 'beranda',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Profil"
+        component={Profil}
+        options={{
+          tabBarLabel: 'Profil',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Pengaturan"
+        component={Pengaturan}
+        options={{
+          tabBarLabel: 'Pengaturan',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="cog" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
   );
 };
 
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tab"
+          component={MenuTab}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default App;
 
-const styles = StyleSheet.create({});
+const style = StyleSheet.create({});
