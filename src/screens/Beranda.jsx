0dd3a341b53d15ceca63ebdfd97d6e1a26e38528
Taufiqurrohman58 <taufiqurrohman250503@gmail.com>
2024-05-04 03:54:29 -0700

    Menambahkan gambar di menu beranda

--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,14 +1,39 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity, ImageBackground} from 'react-native';
 import React from 'react';
 
-const Beranda = () => {
+const Beranda = ({navigation}) => {
   return (
     <View>
-      <Text>Beranda</Text>
+      <ImageBackground source={require('../assets/logo.jpg')} style={styles.background}>
+        <Text style={styles.isitext1}>FIQUR CELL</Text>
+        <Text style={styles.isitext2}>Melayani e-transaksi</Text>
+      </ImageBackground>
+ 
     </View>
   );
 };
 
 export default Beranda;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'green',
+    padding: 20,
+    height: 150,
+    borderRadius: 10,
+  },
+
+  isitext1: {
+    color:'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginTop:30,
+    textAlign: 'center',
+  },
+  isitext2: {
+    color:'white',
+    fontSize: 12,
+    textAlign: 'center',
+  },
+});
