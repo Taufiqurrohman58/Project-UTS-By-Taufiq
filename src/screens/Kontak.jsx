f64d984c4252b1631dc61d62a24c9ae474e32025
Taufiqurrohman58 <taufiqurrohman250503@gmail.com>
2024-05-05 05:30:50 -0700

    menambahkan isi pada menu kontak

--- src/screens/Kontak.jsx
+++ src/screens/Kontak.jsx
@@ -1,14 +1,54 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import { View, Text, Button, Linking, Image, StyleSheet } from 'react-native';
 
-const Kontak = () => {
+const ContactScreen = () => {
+  const handleCall = () => {
+    Linking.openURL('tel:6285947719207');
+  };
+
+  const handleWhatsApp = () => {
+    Linking.openURL('https://api.whatsapp.com/send?phone=+6285947719207');
+  };
+
   return (
-    <View>
-      <Text>Kontak</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.jpg')} style={styles.profileImage} />
+      <Text style={styles.title}>Contact Fiqur Cell</Text>
+      <View style={styles.button}>
+        <Button title="Call" color ='green' onPress={handleCall} />
+      </View>
+      <View>
+        <Button title="WhatsApp" color ='green' onPress={handleWhatsApp} />
+
+      </View>
     </View>
   );
 };
 
-export default Kontak;
+const styles = StyleSheet.create({
 
-const styles = StyleSheet.create({});
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop:40,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 75,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    color:'green',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
+
+export default ContactScreen;
